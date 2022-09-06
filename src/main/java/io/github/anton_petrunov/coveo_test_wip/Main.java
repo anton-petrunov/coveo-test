0fa8a51d08b632efbd84cfa2975ae7d6bfb7b3ae
Anton Petrunov <a.n.tone@mail.ru>
2022-09-06 12:29:45 +0300

    Update working algorithms in Main

--- src/main/java/io/github/anton_petrunov/coveo_test_wip/Main.java
+++ src/main/java/io/github/anton_petrunov/coveo_test_wip/Main.java
@@ -7,10 +7,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Main {
+    final static String completeSearchBody = "London";
+    final static String partialSearchBody = "ondo";
+
     public static void main(String[] args) throws IOException {
         List<City> cities = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("cities_canada-usa.tsv"))) {
@@ -19,8 +21,18 @@ public class Main {
                 cities.add(parse(bufferedReader.readLine()));
             }
         }
-        System.out.println(cities);
-        System.out.println(cities.stream().filter(city -> Objects.equals(city.getName(), "London")).collect(Collectors.toList()));
+
+//        System.out.println(cities);
+        List<City> citiesCompleteMathByName = cities.stream()
+                .filter(city -> city.getName().matches(completeSearchBody)).collect(Collectors.toList());
+        System.out.println("Quantity of cities with complete match " + citiesCompleteMathByName.size());
+
+        List<City> citiesPartialMatchByName = cities.stream().filter(
+                city -> city.getName().contains(partialSearchBody)).collect(Collectors.toList());
+        System.out.println("Quantity of cities with partial match " + citiesPartialMatchByName.size());
+
+        System.out.println(cities.stream().
+                filter(city -> city.getName().matches("\\D{3}")).collect(Collectors.toList()));
     }
 
     public static City parse(final String line) {
