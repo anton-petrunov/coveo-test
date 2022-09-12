daa7c92e01cb4d865555523c14037fa95bd866b9
Anton Petrunov <a.n.tone@mail.ru>
2022-09-12 16:30:18 +0300

    Update SpringMain and delete Main

--- src/main/java/io/github/anton_petrunov/coveo_test_wip/SpringMain.java
+++ src/main/java/io/github/anton_petrunov/coveo_test_wip/SpringMain.java
@@ -1,22 +1,19 @@
 package io.github.anton_petrunov.coveo_test_wip;
 
 import io.github.anton_petrunov.coveo_test_wip.repository.InMemoryCityRepository;
+import io.github.anton_petrunov.coveo_test_wip.service.CityService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static io.github.anton_petrunov.coveo_test_wip.util.CityUtil.getTos;
-
 public class SpringMain {
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "classpath:spring/spring-app.xml")) {
             InMemoryCityRepository cityRepository = applicationContext.getBean(InMemoryCityRepository.class);
-            System.out.println(cityRepository.getCitiesPartialMatchingNames("London").size());
 
-            System.out.println(cityRepository.getCitiesCompleteMatchingNames("London").size());
-            System.out.println(getTos(
-                    cityRepository.getCitiesPartialMatchingNames("London"),
-                    43.70011F,
-                    -79.4163F));
+            CityService service = applicationContext.getBean(CityService.class);
+
+            service.findAndScore("Londo", 43.70011F, -79.4163F).forEach(System.out::println);
         }
     }
 }
