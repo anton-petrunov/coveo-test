6fb086e8fa3be7fdd69674d6e6cbd47be8fce13c
Anton Petrunov <a.n.tone@mail.ru>
2022-09-12 16:23:16 +0300

    Update CityTo and CityUtil

--- src/main/java/io/github/anton_petrunov/coveo_test_wip/to/CityTo.java
+++ src/main/java/io/github/anton_petrunov/coveo_test_wip/to/CityTo.java
@@ -3,40 +3,86 @@ package io.github.anton_petrunov.coveo_test_wip.to;
 public class CityTo {
     private final Integer geoNameId;
     private final String name;
-    private final Float Latitude;
+    private Float nameScore;
+    private final Float latitude;
     private final Float longitude;
     private final String country;
     private final String admin1;
     private Float distanceKm;
+    private Float distanceScore;
+    private Float score;
 
-    public CityTo(Integer geoNameId, String name, Float latitude, Float longitude, String country, String admin1, float distanceKm) {
+    public CityTo(Integer geoNameId, String name, Float latitude, Float longitude, String country, String admin1) {
+        this(geoNameId, name, 0F, latitude, longitude, country, admin1, 0F, 0F, 0F);
+    }
+
+    public CityTo(Integer geoNameId, String name, Float nameScore, Float latitude, Float longitude, String country, String admin1, Float distanceKm, Float distanceScore, Float score) {
         this.geoNameId = geoNameId;
         this.name = name;
-        Latitude = latitude;
+        this.nameScore = nameScore;
+        this.latitude = latitude;
         this.longitude = longitude;
         this.country = country;
         this.admin1 = admin1;
         this.distanceKm = distanceKm;
+        this.distanceScore = distanceScore;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
     }
 
     public Float getDistanceKm() {
         return distanceKm;
     }
 
+    public Float getNameScore() {
+        return nameScore;
+    }
+
+    public Float getDistanceScore() {
+        return distanceScore;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
     public void setDistanceKm(float distanceKm) {
         this.distanceKm = distanceKm;
+    }
+
+    public void setNameScore(Float nameScore) {
+        this.nameScore = nameScore;
+    }
+
+    public void setDistanceScore(Float distanceScore) {
+        this.distanceScore = distanceScore;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
     }
 
     @Override
     public String toString() {
         return "CityTo{" +
-                "geoNameId=" + geoNameId +
-                ", name='" + name + '\'' +
-                ", Latitude=" + Latitude +
-                ", longitude=" + longitude +
+                "name='" + name + '\'' +
+                ", nameScore=" + nameScore +
                 ", country='" + country + '\'' +
                 ", admin1='" + admin1 + '\'' +
                 ", distanceKm=" + distanceKm +
+                ", distanceScore=" + distanceScore +
+                ", score=" + score +
                 '}';
     }
 }
--- src/main/java/io/github/anton_petrunov/coveo_test_wip/util/CityUtil.java
+++ src/main/java/io/github/anton_petrunov/coveo_test_wip/util/CityUtil.java
@@ -3,30 +3,25 @@ package io.github.anton_petrunov.coveo_test_wip.util;
 import io.github.anton_petrunov.coveo_test_wip.model.City;
 import io.github.anton_petrunov.coveo_test_wip.to.CityTo;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static io.github.anton_petrunov.coveo_test_wip.util.DistanceUtil.calculateDistanceKm;
-
 public class CityUtil {
-    private static CityTo createTo(City city, float searchPointLatitude, float searchPointLongitude) {
+
+    private static CityTo createTo(City city) {
         return new CityTo(
                 city.getGeoNameId(),
                 city.getName(),
                 city.getLatitude(),
                 city.getLongitude(),
                 city.getCountry(),
-                city.getAdmin1(),
-                calculateDistanceKm(searchPointLatitude, searchPointLongitude,
-                        city.getLatitude(), city.getLongitude())
+                city.getAdmin1()
         );
     }
 
-    public static List<CityTo> getTos(List<City> cities, float searchPointLatitude, float searchPointLongitude) {
+    public static List<CityTo> createTos(List<City> cities) {
         return cities.stream()
-                .map(city -> createTo(city, searchPointLatitude, searchPointLongitude))
-                .sorted(Comparator.comparing(CityTo::getDistanceKm))
+                .map(CityUtil::createTo)
                 .collect(Collectors.toList());
     }
 }
